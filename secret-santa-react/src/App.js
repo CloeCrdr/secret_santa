a9faa48369de4274f11eb58ae9b2f0a1215c0ae8
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2022-09-29 09:36:46 +0200

    add profile page and design to  navbar and body

--- secret-santa-react/src/App.js
+++ secret-santa-react/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './components/Home';
+import Profile from './components/Profile';
 import {BrowserRouter, NavLink, Routes, Route, Redirect} from 'react-router-dom';
 
 
@@ -9,14 +10,21 @@ function App() {
       <div className="App">
         <header className="App-header">
           <nav className="navbar">
+            
             <NavLink to="/home">Home</NavLink>
+            <NavLink to="/profile">Profile</NavLink>
             <NavLink>About</NavLink>
             <NavLink>Contact</NavLink>
           </nav>
-          <Routes>
-              <Route path="/home" element={<Home/>} />
-          </Routes>
+          <nav className="navLog">
+            <NavLink>Login</NavLink>
+            <NavLink>Register</NavLink>
+          </nav>
         </header>
+        <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
---
+++ secret-santa-react/src/components/Profile.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+const Profile= () => {
+    return (
+        <div>
+            <h1>Profile test</h1>
+        </div>
+    )
+}
+export default Profile;
