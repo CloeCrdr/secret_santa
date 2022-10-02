ce4e2201e7cec26859dafa6eb33cdd0d1845dd84
RandaSan <103753995+RandaSan@users.noreply.github.com>
2022-10-02 22:28:09 +0200

    Add files via upload

--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const connectionBdd = require("../config/db");
 
+// routes to get groups and users infos 
 router.get("/", (req, res) => {
     return new Promise (() => {
         connectionBdd.query("SELECT g.id,g.nom,(SELECT CONCAT(user.prenom,' ',user.nom)FROM user WHERE user.id = g.admin_id) AS admin FROM groupe AS g", (err, resp) => {
@@ -11,6 +12,7 @@ router.get("/", (req, res) => {
     })
 })
 
+//// route to get group by id
 router.get("/:group_id", (req, res) => {
     return new Promise (() => {
         connectionBdd.query("SELECT gu.id,gu.user_id,gu.groupe_id,g.nom as groupe,u.id,u.nom,u.prenom,u.email FROM groupe_user AS gu LEFT JOIN user AS u ON u.id = gu.user_id LEFT JOIN `groupe` AS g ON g.id = gu.groupe_id WHERE groupe_id = ?", 
@@ -22,6 +24,7 @@ router.get("/:group_id", (req, res) => {
     })
 })
 
+// route to get group informations and user info by user_id
 router.get("/profile/:user_id", (req, res) => {
     return new Promise (() => {
         connectionBdd.query("SELECT * FROM groupe AS g LEFT JOIN groupe_user AS gu ON gu.groupe_id = g.id WHERE gu.user_id = ?",
@@ -33,6 +36,8 @@ router.get("/profile/:user_id", (req, res) => {
     })
 })
 
+
+// route to create a new user
 router.post("/addNew", (req, res) => {
     return new Promise ((result, reject) => {
         connectionBdd.query("INSERT INTO groupe (nom, admin_id) VALUES (?, ?)",
--- server/routes/userRoutes.test.js
+++ server/routes/userRoutes.test.js
@@ -1,14 +1,41 @@
-const userRoutes = require("./userRoutes")
-import request from 'supertest';
+const router = require('./userRoutes')
+const request = require('supertest');
+const mysql = require("mysql2")
 const express = require("express")
-const app = new express();
+const app = express();
+const userData = require('../spec/user.json')
+
+app.use(express.urlencoded({extended: true}))
+app.use('/', router)
+
+jest.fn().mockReturnValueOnce(userData)
 
 describe('User Routes', () => {
-    const user = require("../spec/user.json")
 
-    test("Get all users", async () => {
-        const res = await request(app).get("/user/")
-        expect(200)
-        expect(user.length).toEqual(6)
+    it("Get all users", async () => {
+        await request(app).get("/").expect(200).then(resp => {
+            expect(resp.body.length).toEqual(1)
+        })
+    })
+
+    it("Get user by id", async () => {
+        await request(app).get("/1").expect(200).then(resp => {
+            expect(resp.body[0].id).toEqual(1)
+            expect(resp.body.length).toEqual(1)
+        })
+    })
+
+    it("Get user if mail and password matching", async () => {
+        const data = {
+            email: "test@mail.fr",
+            password: "test"
+        }
+
+        await request(app).get(`/check/${data.email}/${data.password}`)
+        .expect(200)
+        .then(resp => {
+            expect(resp.body[0].email).toEqual("test@mail.fr")
+            expect(resp.body.length).toEqual(1)
+        })    
     })
 })
